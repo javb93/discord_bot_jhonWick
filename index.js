909fa4c371a53e51c63045ffe4621f6ea379790b
Alfredo Vidal <javb9313@gmail.com>
2024-09-30 08:45:37 -0600

    finish commit

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
     console.error(error);
     if (interaction.replied || interaction.deferred) {
       await interaction.followUp({
-        content: "There was an error while executing this command!",
+        content: "There was an error while executing this command! FINISH",
         ephemeral: true,
       });
     } else {
